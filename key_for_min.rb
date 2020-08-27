7d22d8eb2d0658f72b11b09110f3666a60c583a8
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-27 14:11:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,6 @@ veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
 
 
 def key_for_min_value(hash)
-   nu = ""
-   
-  hash.sort_by { |key, value| key }.first
-
-  end
-  
-  def key_for_min_value(hash)
   values = hash.values
   hash.keys[values.index(values.min).to_i]
 end
