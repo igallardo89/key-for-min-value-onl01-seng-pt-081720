712eb320d0148819b531c9c2d4ca8878bd5e4e9d
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-27 14:16:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,17 +3,12 @@ ikea = {:chair => 25, :table => 85, :mattress => 450}
 veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
 
 
-
-def key_for_min_value(ikea)
-    ikea.reduce([nil, nil]){
-        |acc, (key, value)|
-        if acc[0].nil?
-            [key, value]
-        elsif acc[1] > value
-            [key, value]
-        else
-            acc
-        end
-    }.first
-
+def key_for_min_value(name_hash)
+  empty_hash = nil
+   name_hash.collect do |name, value|
+     if value <= 1
+       return name
+   end
+  end
+  empty_hash
 end
