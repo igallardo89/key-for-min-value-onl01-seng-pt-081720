27f85caa6a3379a65b39eaa5aaa111793eab935d
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-27 14:17:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,13 @@ veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
 
 
 def key_for_min_value(name_hash)
-  empty_hash = nil
-   name_hash.collect do |name, value|
-     if value <= 1
-       return name
-   end
+ smallest_value = 0
+ empty_hash = nil
+ name_hash.each do |name, value|
+  if smallest_value == 0 || value < smallest_value
+    smallest_value = value
+    empty_hash = name
   end
-  empty_hash
+ end
+ empty_hash
 end
