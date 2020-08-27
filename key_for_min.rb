f0f4648d255659261004db1891b95d9a98fedee1
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-27 14:12:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@ veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
 
 
 
-def key_for_min_value(hash)
-  values = hash.values
-  hash.keys[values.index(values.min).to_i]
+def key_for_min_value(ikea)
+  values = ikea.values
+  ikea.keys[values.index(values.min).to_i]
 end
  
