4e6ad81414c45df10a22263074288ae6b0d46ce1
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-27 14:11:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,4 +10,9 @@ def key_for_min_value(hash)
   hash.sort_by { |key, value| key }.first
 
   end
+  
+  def key_for_min_value(hash)
+  values = hash.values
+  hash.keys[values.index(values.min).to_i]
+end
  
